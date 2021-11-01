097d26929b8874905d62d5a4a395311ede0b325e
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-01 23:15:28 +0100

    renderConditionally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import ReactDom from 'react-dom';
-import Login from '../Login';
+// import ReactDom from 'react-dom';
+import Login from "../Login";
 
 let isLoggedIn = false;
 
 function renderConditionally() {
-  if(isLoggedIn) return <h1>Hello</h1>
-else {
-  return (
-  <login />);
+  if (isLoggedIn) {
+    return <h1>Hello</h1>;
+  } else {
+    return <login />;
+  }
 }
 
 function App() {
-  return
-    (<div className="container">
-      {renderConditionally}
-    </div>)
-, document.getElementById('root')}
+  return;
+  <div className="container">{renderConditionally()}</div>;
+}
 
 export default App;
